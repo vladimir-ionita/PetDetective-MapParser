3e8aeb503e18c59ac3957e8453cbed89e28c9694
Vladimir Ionita <ionita.vladimir@gmail.com>
2020-05-27 23:52:03 +0000

    Find nodes coordinates by grouping locations

--- intersection_nodes_detection.py
+++ intersection_nodes_detection.py
@@ -9,4 +9,23 @@ def detect_intersection_nodes(image, intersection_node_image):
 
     coordinates_split = locations[::-1]
     coordinates = list(zip(*coordinates_split))
-    
+
+    node_coordinates = [coordinates.pop(0)]
+    for coord in coordinates:
+        if coordinates_far_enough(coord, node_coordinates):
+            node_coordinates.append(coord)
+    return node_coordinates
+
+
+def coordinates_far_enough(coordinates, all_coordinates):
+    for coord in all_coordinates:
+        if distance_between_points(coord, tuple(coordinates)) < 20:
+            return False
+    return True
+
+
+def distance_between_points(p1, p2):
+    x1, y1 = p1
+    x2, y2 = p2
+    return math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+
--- main.py
+++ main.py
@@ -6,4 +6,6 @@ if __name__ == "__main__":
     map_image = cv2.imread("resources/maps/IMG_9931.PNG")
     intersection_node_image = cv2.imread("resources/map_parts/intersection.png")
 
-    detect_intersection_nodes(map_image, intersection_node_image)
+    nodes = detect_intersection_nodes(map_image, intersection_node_image)
+    print(nodes)
+
