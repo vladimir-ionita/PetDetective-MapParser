692c84d04d8a2be89e5a616be1a7fd16c320bd12
Vladimir Ionita <ionita.vladimir@gmail.com>
2020-05-30 02:32:30 +0000

    Use `get_intersection_nodes_coordinates` in main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from intersection_nodes_detection import *
+from image_processing.intersection_nodes_detection_v2 import get_intersection_nodes_coordinates_v2
+from image_processing.intersection_nodes_detection import get_intersection_nodes_coordinates
+import constants
 import cv2
 
 
@@ -6,6 +8,9 @@ if __name__ == "__main__":
     map_image = cv2.imread("resources/maps/IMG_9931.PNG")
     intersection_node_image = cv2.imread("resources/map_parts/intersection.png")
 
-    nodes = detect_intersection_nodes(map_image, intersection_node_image)
+    nodes = get_intersection_nodes_coordinates(map_image, intersection_node_image)
+    nodes.sort()
     print(nodes)
-
+    nodes = get_intersection_nodes_coordinates_v2(map_image, constants.Colors.intersection_node_color, True)
+    nodes.sort()
+    print(nodes)
